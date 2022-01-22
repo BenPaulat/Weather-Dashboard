d43ce3e9257f22f5713c009f7cab5a0f4859f56e
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-22 16:26:00 -0600

    created form input elements and added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
 // VARIABLES
+var searchFormEl = document.querySelector('#search-form');
+var cityInputEl = document.querySelector('#city');
+
 // API key
 const apiKey = "59821731ddfaee6faca13d91cee4cbbc";
 // OneCall API URL
 var oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclusions}&appid=${apiKey}`;
 // Current Weather API URL
 var currentWeatherAPI = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+// Search submission handler
+function searchSubmitHandler() {
+    event.preventDefault();
+
+    // get value from input element
+    var city = cityInputEl.value.trim();
+
+    if (city) {
+        getWeatherData(city);
+
+        // clear old content
+        weatherContainerEl.textContent = "";
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a city");
+    }
+};
 
 // fetch function for city search data
 function getWeatherData(city) {
@@ -33,3 +54,5 @@ function getWeatherData(city) {
 // function to parse search results
 
 // function to generate cards of weather data
+
+searchFormEl.addEventListener("submit", searchSubmitHandler);
