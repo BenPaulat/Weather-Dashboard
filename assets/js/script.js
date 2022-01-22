c42c47945f7ce806dc450c057a7208ab0801ed36
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-22 16:11:56 -0600

    added getWeatherData function to fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,33 @@
 const apiKey = "59821731ddfaee6faca13d91cee4cbbc";
 // OneCall API URL
 var oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclusions}&appid=${apiKey}`;
-// exclusions
-var exclusions = "";
-// Latitude and Longitude variables
-var lat = "";
-var lon = "";
 // Current Weather API URL
-var currentWeatherAPI = `api.openweathermap.org/data/2.5/weather?q=${city},${stateCode}&appid=${apiKey}`;
-// City and State variables
-var city = "";
-var stateCode = "";
+var currentWeatherAPI = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+// fetch function for city search data
+function getWeatherData(city) {
+    let currentWeatherAPI = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+    fetch(currentWeatherAPI).then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+                parseResponse(data);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+        .catch(function (error) {
+            alert("Unable to connect to Weather API");
+        });
+};
+
+// generate array of previous search cities
+
+// set local storage function for previous searches
+
+// function to parse search results
+
+// function to generate cards of weather data
